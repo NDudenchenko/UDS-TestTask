2305ebeca5e283c17425f808d7eab478e0bb5df9
NDudenchenko <37368829+NDudenchenko@users.noreply.github.com>
2020-08-05 18:12:16 +0300

    Add project files.

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            if (dayCount <= 0)
+            {
+                throw new Exception("Day count must be positive integer");
+            }
+
+            DateTime preliminaryResultDate = startDate.AddDays(dayCount - 1);
+            if (weekEnds != null)
+            {
+                for (int i = 0; i < weekEnds.Length; i++)
+                {
+                    if (DateTime.Compare(weekEnds[i].StartDate, preliminaryResultDate) <= 0 &&
+                        DateTime.Compare(weekEnds[i].StartDate, startDate) >= 0)
+                    {
+                        int daysShift = weekEnds[i].EndDate.Day - weekEnds[i].StartDate.Day + 1;
+                        preliminaryResultDate = preliminaryResultDate.AddDays(daysShift);
+                    }
+                }
+
+                return preliminaryResultDate;
+            }
+
+            return preliminaryResultDate;
+        }
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CSharpTest;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharpTest.WorkDayCalculatorTests tests = new WorkDayCalculatorTests();
+            tests.TestNoWeekEnd();
+            tests.TestNormalPath();
+            tests.TestNormalPathMultiplyWeekends();
+            tests.TestWeekendAfterEnd();
+        }
+    }
+}
